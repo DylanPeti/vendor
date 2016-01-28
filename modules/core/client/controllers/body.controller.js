5663997181bc679d4d95338c1f8ad115495f0674
Dylan <dylanpetiinfo@gmail.com>
2016-01-29 08:22:04 +1300

    Position Carousel indicators and add captions

--- modules/core/client/controllers/body.controller.js
+++ modules/core/client/controllers/body.controller.js
@@ -8,12 +8,35 @@ angular.module('vendor').controller('BodyController', ['$scope', '$location', 'A
 		// if (!$scope.authentication.user) $location.path('/authentication/signin');
 		$scope.welcome = "welcome";
 
+		// $scope.Images = [
+		// '/core/client/img/gift-cards.jpg', 
+		// '/core/client/img/register.jpg',
+		// '/core/client/img/cash-management.jpg',
+		// '/core/client/img/vend-u.jpg'
+		// ];
+
+
+
 		$scope.Images = [
-		'/core/client/img/gift-cards.jpg', 
-		'/core/client/img/register.jpg',
-		'/core/client/img/cash-management.jpg',
-		'/core/client/img/vend-u.jpg'
+		{
+			image: '/core/client/img/gift-cards.jpg', 
+			caption: "Attract new customers & increase revenue with gift cards."
+		},
+		{
+			image: '/core/client/img/cash-management.jpg', 
+			caption: "Track all cash movements from cash float to register closure."
+		},
+		{
+			image: '/core/client/img/register.jpg', 
+			caption: "The beautifully fast web-based checkout experience. Try it now!"
+		}, 
+		{
+			image: '/core/client/img/vend-u.jpg', 
+			caption: "Retail ebooks, guides, live events, webinars and training."
+		}
 		];
+
+
 
 
 		
